3ee6032a95fa96df41a48a5a9f71cada454c2086
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-03-17 00:06:52 +0700

    + add custom tailwind style, scss style
    + layout header, footer

---
+++ src/app/shopping/layout/header-topbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'esa-shopping-header-topbar',
+    templateUrl: './header-topbar.component.html'
+})
+export class HeaderTopbarComponent {
+}
--- src/app/shopping/shopping.module.ts
+++ src/app/shopping/shopping.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from "@angular/core";
 import { shoppingRoutes } from "./shopping.routes";
 import { RouterModule } from "@angular/router";
+import { HeaderTopbarComponent } from "./layout/header-topbar.component";
+import { FooterComponent } from "./layout/footer.component";
+import { ShoppingComponent } from "./shopping.component";
 
 @NgModule({
     imports: [
         RouterModule.forChild(shoppingRoutes)
     ],
-    declarations: [],
+    declarations: [
+        ShoppingComponent,
+        HeaderTopbarComponent,
+        FooterComponent
+    ],
     exports: [],
 })
 export class ShoppingModule {
