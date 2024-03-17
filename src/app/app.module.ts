4668e9d32f97e688c4cfc77035c52eef3629d04b
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-03-17 13:00:09 +0700

    + config ngrx thru npm and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
+import { NgModule, isDevMode } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { RouterModule } from "@angular/router";
 import { appRoutes } from "./app.routes";
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 @NgModule({
     imports: [
         CommonModule,
         BrowserModule,
-        RouterModule.forRoot(appRoutes)       
+        RouterModule.forRoot(appRoutes),
+        StoreModule.forRoot({}),
+        EffectsModule.forRoot([]),
+        StoreDevtoolsModule.instrument({
+            maxAge: 25,
+            logOnly: !isDevMode(),
+            autoPause: true,
+            traceLimit: 25
+        })
     ],
     declarations: [AppComponent],
     exports: [],
