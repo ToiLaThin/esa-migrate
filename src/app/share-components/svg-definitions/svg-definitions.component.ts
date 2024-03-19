ee49c4d13d7bf9317f26f22e5202c9b4dfde2da7
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-03-19 16:56:01 +0700

    + add reusable svg-icon, definition with variable size, fill color, load
    symbol svg

---
+++ src/app/share-components/svg-definitions/svg-definitions.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'svg-definitions',
+    templateUrl: 'svg-definitions.component.html',
+    styleUrls: []
+})
+export class SvgDefinitionsComponent {}
