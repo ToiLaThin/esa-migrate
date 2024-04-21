15fee28476e5babba7e751f5add80573429db39a
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-04-21 23:49:06 +0700

    translate index action shopnow

--- src/app/shopping/components/index/banner/banner.component.ts
+++ src/app/shopping/components/index/banner/banner.component.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
+import { I18NCommonIdSelector } from "../../../../core/translation-loader/i18n-common-id";
 
 @Component({
     selector: 'esa-banner',
     templateUrl: './banner.component.html'
 })
 export class BannerComponent {
-
+    get I18NCommonIds() {
+        return I18NCommonIdSelector;
+    }
 }
