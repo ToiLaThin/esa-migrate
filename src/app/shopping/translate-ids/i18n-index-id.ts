e3bca9866ee36b26732968335b05c3c8f4d337e4
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-04-28 08:39:54 +0700

    + add translation, index , layout footer
    + add assets, add icon

---
+++ src/app/shopping/translate-ids/i18n-index-id.ts
@@ -0,0 +1,28 @@
+export enum I18NIndexIdSelector {
+    //Banner
+    Banner1Top = 'index.banner.banner1_top',
+    Banner1Mid = 'index.banner.banner1_mid',
+    Banner1Bottom = 'index.banner.banner1_bottom',
+
+    Banner2Top = 'index.banner.banner2_top',
+    Banner3Top = 'index.banner.banner3_top',
+
+
+    //Body
+    BodyFreeShipping = 'index.body.free_shipping',
+    BodyFreeShippingDesc = 'index.body.free_shipping_desc',
+    BodyOnlyPrice = 'index.body.only_price',
+    Banner4Top = 'index.body.banner4_top',
+    Banner5Top = 'index.body.banner5_top',
+    Banner6Top = 'index.body.banner6_top',
+    Banner7Top = 'index.body.banner7_top',
+    Banner7Mid = 'index.body.banner7_mid',
+    Banner8Top = 'index.body.banner8_top',
+    Banner8Mid = 'index.body.banner8_mid',
+    TabDailyProduct = 'index.body.tab_daily_product',
+    TabDrinks = 'index.body.tab_drinks',
+    TabMilks = 'index.body.tab_milks',
+    TabFreshFood = 'index.body.tab_fresh_food',
+    TabHomeAppliance = 'index.body.tab_home_appliance',
+
+}
--- src/app/shopping/translate-ids/i18n-layout-id.ts
+++ src/app/shopping/translate-ids/i18n-layout-id.ts
@@ -1,5 +1,6 @@
 export enum I18NLayoutIdSelector {
     //Topbar
+    LayoutTopbarWelcome = 'layout.topbar.welcome',
     LayoutTopbarLogin = 'layout.topbar.login',
     LayoutTopbarLogout = 'layout.topbar.logout',
     LayoutTopbarRegister = 'layout.topbar.register',
@@ -16,8 +17,43 @@ export enum I18NLayoutIdSelector {
 
     //Navbar
     LayoutNavbarHome = 'layout.nav.home',
+    LayoutNavbarSales = 'layout.nav.sales',
+    LayoutNavbarNewArrivals = 'layout.nav.new_arrivals',
+    LayoutNavbarAllBrands = 'layout.nav.all_brands',
     LayoutNavbarAbout = 'layout.nav.about',
     LayoutNavbarContact = 'layout.nav.contact',
     LayoutNavbarMore = 'layout.nav.more',
     LayoutNavbarShopCatalog = 'layout.nav.shop_catalog',
+    LayoutNavbarFreeShipping = 'layout.nav.free_shipping',
+
+    //Footer
+    FooterContactUsHeading = 'layout.footer.contact_us.heading',
+    FooterContactUsAddress = 'layout.footer.contact_us.address',
+    FooterContactUsPhone = 'layout.footer.contact_us.phone',
+    FooterContactUsEmail = 'layout.footer.contact_us.email',
+    FooterContactUsChat = 'layout.footer.contact_us.chat',
+
+    FooterProductHeading = 'layout.footer.product.heading',
+    FooterProductPriceDrops = 'layout.footer.product.price_drops',
+    FooterProductNewProducts = 'layout.footer.product.new_products',
+    FooterProductBestSales = 'layout.footer.product.best_sales',
+    FooterProductContactUs = 'layout.footer.product.contact_us',
+    FooterProductSitemap = 'layout.footer.product.sitemap',
+    FooterProductStores = 'layout.footer.product.stores',
+
+
+    FooterOurCompanyHeading = 'layout.footer.our_company.heading',
+    FooterOurCompanyDelivery = 'layout.footer.our_company.delivery',
+    FooterOurCompanyLegalNotice = 'layout.footer.our_company.legal_notice',
+    FooterOurCompanyTermAndConditions = 'layout.footer.our_company.terms_and_conditions',
+    FooterOurCompanyAboutUs = 'layout.footer.our_company.about_us',
+    FooterOurCompanySecurePayment = 'layout.footer.our_company.secure_payment',
+    FooterOurCompanyPrivacyPolicy = 'layout.footer.our_company.privacy_policy',
+
+    FooterSubscribeToNewsletterHeading = 'layout.footer.subscribe_to_newsletter.heading',
+    FooterSubscribeToNewsletterDescription = 'layout.footer.subscribe_to_newsletter.description',
+    FooterSubscribeToNewsletterEmailPlaceholder = 'layout.footer.subscribe_to_newsletter.email_placeholder',
+    FooterSubscribeToNewsletterSubscribe = 'layout.footer.subscribe_to_newsletter.subscribe',
+    FooterSubscribeToNewsletterAgreeTermConditions = 'layout.footer.subscribe_to_newsletter.agree_term_conditions',
+
 }
